7d38888703ef22758e528dc03c9ed2f907d02352
mtbjj <m-babej@onu.edu>
2015-04-14 09:38:30 -0400

    Added my graphing test program to project

---
+++ Curve_Knee_Finder/src/curvekneecalcpackage/Graph.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package curvekneecalcpackage;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author mtbjj
+ */
+public class Graph extends Canvas{
+    
+    Graph() {
+         
+    }
+    
+    void drawLine(Point p1, Point p2, int graphHeight) {
+        Graphics g = getGraphics();
+        g.drawLine(p1.getX(), graphHeight -p1.getY(), p2.getX(), graphHeight -p2.getY());
+    }
+}
---
+++ Curve_Knee_Finder/src/curvekneecalcpackage/Point.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package curvekneecalcpackage;
+
+/**
+ *
+ * @author mtbjj
+ */
+public class Point {
+    private final int x;
+    private final int y;
+    
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
+}
