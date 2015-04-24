d2e64e7160b4f79a833ad241b0fed50d5069c16c
wetbug75 <a-krause@onu.edu>
2015-04-24 10:50:16 -0400

    Added all Tool images and edited Node and Tool classes

--- MP5_CircuitLesson/src/Node.java
+++ MP5_CircuitLesson/src/Node.java
@@ -9,5 +9,54 @@
  * @author Andrew
  */
 public class Node {
+    static enum Type{
+        WIRE, RESISTOR, BATTERY, CAPACITOR;
+    }
+    //WIRE = 1;
+    //RESISTOR = 2;
+    //BATTERY = 3;
+    //CAPACITOR = 4;
     
+    private int volts;
+    private int resistance;
+    Type type;
+    //osciliscope vars?
+    
+    Node(Type thing, int voltValue, int resistValue/*, osciliscope vars*/)
+    {
+        type = thing;
+        volts = voltValue;
+        resistance = resistValue;
+        //osciliscope?
+    }
+    
+    public Type getType()
+    {
+        return type;
+    }
+    
+    public int getVolts()
+    {
+        return volts;
+    }
+    
+    public int getResistance()
+    {
+        return resistance;
+    }
+    
+    /*public void graphOsciliscope()
+    {
+        ???
+    }*/
+    
+    public void setVolts(int v)
+    {
+        volts = v;
+    }
+    
+    public void setResistance(int r)
+    {
+        resistance = r;
+    }
 }
--- MP5_CircuitLesson/src/Tool.java
+++ MP5_CircuitLesson/src/Tool.java
@@ -9,5 +9,27 @@
  * @author Andrew
  */
 public class Tool {
+    //static final int WIRE = 1;
+    //static final int RESISTOR = 2;
+    //static final int BATTERY = 3;
+    //static final int CAPACITOR = 4;
     
+    int value;
+    
+    Tool()
+    {
+        value = 0;
+    }
+    
+    public String display()
+    {
+        if(value == -1)
+            return "ERROR";
+        return "" + value;
+    }
+    
+    public void setValue(Node part)
+    {
+        
+    }
 }
